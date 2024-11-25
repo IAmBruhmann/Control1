baa621274eb599f60cfba5ac7883f0c78de67aa2
IAmBruhmann <ivantulaew@yandex.ru>
2024-11-25 11:49:14 +0300

    final100

--- index.js
+++ index.js
@@ -40,12 +40,8 @@ class PrintEditionItem {
     }
 //4.2
     fix() {
-        if (this.state < 100) {
-            this.state *= 1.5;
-            if (this.state > 100) {
-                this.state = 100;
-            }
-        }
+            this.state  = this._state * 1.5;
+            
     }
 //4.3
     set state(newState) {
@@ -85,3 +81,5 @@ console.log(book);
 
 book.fix();
 console.log(book.state);
+
+book.state = 1000
